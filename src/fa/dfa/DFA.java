c255b4b9fec850f5a13a1948a1d4becf1035b768
Jonathan Titpon <jonathantipton@u.boisestate.edu>
2020-02-16 06:27:58 -0700

    Transition Function Changes
    - changed transitionMap to utilize two Maps
    - created addTransition function
    - minor changes to other methods for clarity

--- src/fa/dfa/DFA.java
+++ src/fa/dfa/DFA.java
@@ -20,93 +20,138 @@ public class DFA implements DFAInterface {
 	int TotalStatesCount = 0;
 	*/
 	
-	//transition function written as "a0a"
-	//use String as key value containing currentState.name and 
-	//concatenate with transition value, return new DFAState
-	Map<String, DFAState> transitionMap = new HashMap<String, DFAState>();
-	
-	//must use a concrete class that implements java.util.Set interface to represent DFA elements
-	Set<DFAState> finalSet = new HashSet<DFAState>();
-	Set<DFAState> stateSet = new HashSet<DFAState>();
+	//Transition functions
+	Map<String, Map<String, String>> transitionMap = new HashMap<>();
+	//States
+	Set<DFAState> finalStates = new HashSet<>();
+	Set<DFAState> states = new HashSet<>();
+	Set<Character> alphabet = new HashSet<>();
 	DFAState startState;
 	DFAState currentState;
 	
-	
-	public void addFinalState(String nextToken) {
-		//working with SET so duplicates will not occur
-		addState(nextToken);
-		finalSet.add(new DFAState(nextToken));
+	/**
+	 * Adds a final state to the DFA
+	 * @param name is the label of the state
+	 */
+	public void addFinalState(String name) {
+		//create new Final state, add to final and regular state sets
+		DFAState newFinal = new DFAState(name);
+		finalStates.add(newFinal);
+		states.add(newFinal);
 		
+		//addState(nextToken);
 		//finalSet.add(nextToken);
 		/**
 		Final[FinalStatesCount] = new DFAState(nextToken);
 		FinalStatesCount++;
 		addState(nextToken);*/
-		
 	}
 
+	
+	/**
+	 * Adds the initial state to the DFA instance
+	 * @param name is the label of the start state
+	 */
 	public void addStartState(String startStateName) {
-		addState(startStateName);
 		startState = new DFAState(startStateName);
+		states.add(startState);
 	}
 
-	public void addState(String nextToken) {
-		stateSet.add(new DFAState(nextToken));
+	
+	/**
+	 * Adds a non-final, not initial state to the DFA instance
+	 * @param name is the label of the state 
+	 */
+	public void addState(String name) {
+		states.add(new DFAState(name));
 		
 		/**
 		StateList[TotalStatesCount] = new DFAState(nextToken);
 		TotalStatesCount++;*/
-		
 	}
 
-	public void addTransition(String valueOf, char c, String valueOf2) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * Adds the transition to the DFA's delta data structure
+	 * Adds the alphabet symbol to t he DFA's alphabet set
+	 * @param fromState is the label of the state where the transition starts
+	 * @param onSymb is the symbol from the DFA's alphabet.
+	 * @param toState is the label of the state where the transition ends
+	 */
+	public  void addTransition (String fromState, char onSymb, String toState) {
+		Map<String, String> newTransition = new HashMap<>();
+		//Create onSymb -> toState transition
+		newTransition.put(Character.toString(onSymb), toState);
+		//create fromState -> newTransition transition
+		transitionMap.put(fromState, newTransition);	
+		//add onSymb to alphabet set
+		alphabet.add(onSymb);
 	}
 
+	/**
+	 * Computes a copy of this DFA
+	 * which language is the complement
+	 * of this DFA's language.
+	 * @return a copy of this DFA
+	 */
 	public DFA complement() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public boolean accepts(String input) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
-	@Override
+	/**
+	 * Getter for Q
+	 * @return a set of states that FA has
+	 */
 	public Set<? extends State> getStates() {
-		// TODO Auto-generated method stub
-		return null;
+		return states;
 	}
 
-	@Override
+	/**
+	 * Getter for F
+	 * @return a set of final states that FA has
+	 */
 	public Set<? extends State> getFinalStates() {
-		// TODO Auto-generated method stub
-		return null;
+		return finalStates;
 	}
 
-	@Override
-	public State getStartState() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Getter for q0
+	 * @return the start state of FA
+	 */
+	public DFAState getStartState() {
+		return startState;
 	}
 
-	@Override
+	/**
+	 * Getter for Sigma
+	 * @return the alphabet of FA
+	 */
 	public Set<Character> getABC() {
-		// TODO Auto-generated method stub
-		return null;
+		return alphabet;
 	}
 
-	@Override
-	public State getToState(DFAState from, char onSymb) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 	public String toString() {
-		
+		//TODO
 		return null;
+	}
+
+
+	@Override
+	/**
+	 * Uses transition function delta of FA
+	 * @param from the source state
+	 * @param onSymb the label of the transition
+	 * @return the sink state.
+	 */
+	public DFAState getToState(DFAState from, char onSymb) {
+		return null;
+		// TODO Auto-generated method stub
 	}
 
 }
